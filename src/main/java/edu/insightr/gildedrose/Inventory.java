21f8d10bbfefb6ec8e5c23277a78b5c8cf7d086d
nancy <nancyngatcha@yahoo.fr>
2018-12-04 17:52:11 +0100

    Nancy

--- src/main/java/edu/insightr/gildedrose/Inventory.java
+++ src/main/java/edu/insightr/gildedrose/Inventory.java
@@ -1,7 +1,21 @@
 package edu.insightr.gildedrose;
 
-public class Inventory {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Inventory implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    ListView<String> listS;
     private Item[] items;
 
     public Item[] getItems() {
@@ -24,6 +38,7 @@ public class Inventory {
                 new Conjured_Mana_Cake()
         };
     }
+
 
     public void printInventory() {
         System.out.println("***************");
